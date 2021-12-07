80b51e52e7e38c9d3376d2ed44787c02d89b4da3
Sean <p0pra100@gmail.com>
2021-12-07 14:47:33 -0500

    Fixed things

--- src/SchedulingSimulator.java
+++ src/SchedulingSimulator.java
@@ -1,7 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SchedulingSimulator {
     private long time;
+    private Queue<Process> processes = new LinkedList<>();
+    private Queue<Process> readyQueue = new LinkedList<>();
 
     public SchedulingSimulator(){
+        time = 0;
         //constructor
     }
 
