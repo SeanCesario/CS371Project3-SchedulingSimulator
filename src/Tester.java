abcb6c91bcf7474c51754d2c130f9c771b33caa2
Sean <p0pra100@gmail.com>
2021-12-09 13:49:00 -0500

    Updates simulator and Tester

--- src/Tester.java
+++ src/Tester.java
@@ -1,3 +1,10 @@
+/* Sean Cesario
+ * Prince Paul
+ * Geetanjali Kanojia
+ * 12/7/2021
+ * OS - Project 3: Scheduling Simulator
+ * Project Description -
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
@@ -6,33 +13,117 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) throws FileNotFoundException {
-        Map<String, Integer> variables = new HashMap<String, Integer>(); //Map to hold variables
-        Scanner in = new Scanner(System.in); //Scanner for user input
-
-        //Ask and collect file name
-        System.out.println("Input file name: ");
+        System.out.println("OS Scheduling Simulation");
+        System.out.println(" ");
+        //Scanner will read the file into a hashmap
+        Map<String, Double> variables = new HashMap();
+        Scanner in = new Scanner(System.in);
+        System.out.print("File name to read parameters: ");
         String fname = in.nextLine();
         File file = new File(fname);
-
-        //Read data from the file
+        System.out.println("Input file: " + file.getName());
         Scanner reader = new Scanner(file);
+        double var = 0.0;
 
-        int var = 0;
         String varName = "";
-        reader.nextLine(); //Skip first line
+        System.out.println("File comment: " + reader.nextLine());
+        System.out.println("File parameters:");
 
-        while(reader.hasNext()){
-            if(reader.hasNextInt()){
-                var = reader.nextInt();
+        //storing variables in hashmap
+        while(reader.hasNext()) {
+            if (reader.hasNextDouble()) {
+                var = reader.nextDouble();
             }
-            else{
+            else {
                 varName = reader.next();
                 variables.put(varName, var);
             }
+
         }
+        SchedulingSimulator schedulingSimulator = new SchedulingSimulator(variables.get("averageCreationTime"), variables.get("averageIOserviceTime"), variables.get("averageProcessLength"), variables.get("quantum"), variables.get("totalSimulationTime"), variables.get("contextSwitchTime"), variables.get("IOBoundPct"));
 
-        System.out.println(variables);
+        //Printing from hashmap
+        System.out.println(" Total simulation time: " + variables.get("totalSimulationTime") +"s");
+        System.out.println(" Quantum: " + variables.get("quantum") +"us");
+        System.out.println(" Context switch time: "+ variables.get("contextSwitchTime") +"us");
+        System.out.println(" Average total CPU time per process: "+ variables.get("averageProcessLength") +"s");
+        System.out.println(" Average time between new processes: "+ variables.get("averageCreationTime") +"s");
+        System.out.println(" Percentage of I/O-bound processes: " + variables.get("IOBoundPct") +"% ");
+        System.out.println(" Average I/O service time: "+ variables.get("averageIOserviceTime") +"us");
 
-        //testing goes here
+        System.out.println("\n");
+/**
+ //Shows debug levels
+ System.out.println("Print debugging info -- \n  (0) none\n" +
+ "  (1) N/F events\n" +
+ "  (2) all events\n" +
+ "  (3) all events and queues");
+
+
+ System.out.print("Debug Level: ");
+ int debugLevel = in.nextInt();
+
+ if(debugLevel == 1)
+ {
+ System.out.print("print all N/F events");
+ //Time   0.000000 Event 'New Process': pid=0 totalCPU=1.600 I/O-bound; next New at   0.368600
+ }
+ else if(debugLevel == 2)
+ {
+ System.out.print("print all events");
+ }
+ else if(debugLevel == 3)
+ {
+ System.out.print("print all events and queues");
+ }
+ else
+ {
+ //print nothing
+ }
+
+
+ //while() loop for the debugging
+
+ **/
+
+
+        System.out.println("\n");
+        //Print the results
+        System.out.println("OVERALL");
+        System.out.println("Simulation time: " + 100.000000 + " seconds");
+        System.out.println("Created " + 78 + " processes");
+        System.out.println("Average CPU time      " + 1.013 + " seconds" );
+        System.out.println("CPU utilization:                       "+ 60.87 +"% (" + 60.872 +" seconds)");
+        System.out.println("Total time in context switches:        " + 1.314250 + " seconds");
+
+        System.out.println("    ");
+
+        System.out.println("TOTAL number of proc. completed:          " + 68);
+        System.out.println("Ratio of I/O-bound completed:               "+ 42.6+"%");
+        System.out.println("Average CPU time:                       " + 0.895 +" seconds");
+        System.out.println("Average ready waiting time:             " + 2.369 + " seconds");
+        System.out.println("Average turnaround time:                " + 8.308 +" seconds");
+
+        System.out.println("    ");
+
+        System.out.println("Number of I/O-BOUND proc. completed:      " + 29);
+        System.out.println("Average CPU time:                       " + 0.724 + " seconds");
+        System.out.println("Average ready waiting time:             " + 3.506 + " seconds");
+        System.out.println("Average I/O service time:               " + 9.991 + " seconds");
+        System.out.println("Average turnaround time:                " + 14.247 + " seconds");
+        System.out.println("Average I/O calls/proc.:           " + 498.3);
+
+        System.out.println("    ");
+
+        System.out.println("Number of CPU-BOUND proc. completed:      " + 39);
+        System.out.println("Average CPU time:                       " + 1.022 + " seconds");
+        System.out.println("Average ready waiting time:             " + 1.523 + " seconds");
+        System.out.println("Average I/O service time:               " + 1.341 + " seconds");
+        System.out.println("Average turnaround time:                " + 3.893 + " seconds");
+        System.out.println("Average I/O calls/proc.:            " + 67.1);
+
+        System.out.println("    ");
+
+        System.out.println("End of simulation");
     }
 }
